eae84cb43a9557b891a9546eb9e258d8a1ec9eaa
unknown <jcambro@umich.edu>
2019-09-09 11:00:10 -0400

    fixing quotes

--- helloworld.py
+++ helloworld.py
@@ -2,8 +2,8 @@ from random import randint
 a = randint(1,100)
 b = randint(1,100)
 result = a + b
-print (“John Ambrose”)
+print ("John Ambrose")
 print (a)
 print (b)
-print (“Sum = “ + str(result))
-print (“Average = “ + str(result/2.0))
+print ("Sum = " + str(result))
+print ("Average = " + str(result/2.0))
